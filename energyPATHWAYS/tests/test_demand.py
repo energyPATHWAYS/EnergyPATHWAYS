ce0e779ffc99aa19a2b76dc233792c3fa9a41fff
Michael A Cohen <mcohen@rollingturtle.com>
2016-03-25 14:20:15 -0700

    Decent test for loading DemandDrivers+Data

--- energyPATHWAYS/tests/test_demand.py
+++ energyPATHWAYS/tests/test_demand.py
@@ -10,6 +10,16 @@ from energyPATHWAYS.util import *
 
 from profile_tools import * # profile(), timer()
 
+# Note: for now I recommend running this test with only a representative sample of subsectors active for speed reasons.
+# Eventually we will be able to decouple these tests from the U.S. example database and this will no longer be a
+# concern. To use the recommended subset:
+# UPDATE "DemandSubsectors" SET is_active = FALSE;
+# UPDATE "DemandSubsectors" SET is_active = TRUE WHERE id IN (72, 37, 10, 16);
+#
+# If/when you want to reset things back to the original example state, you can use:
+# UPDATE "DemandSubsectors" SET is_active = TRUE;
+# UPDATE "DemandSubsectors" SET is_active = FALSE WHERE id IN (13, 14, 32);
+
 class DemandTest(ut.TestCase):
     
     @classmethod
@@ -43,7 +53,37 @@ class DemandTest(ut.TestCase):
         print 'subsector precursors'
         print demand.subsector_precursors
 
-        # TODO: figure out what to assert here
+        # To start with, we're just testing around DemandDrivers
+        # and relying on the data in the existing us example database
+        self.assertEqual(len(demand.drivers), 34)
+
+        # load up a couple of example drivers and make sure their properties and data match expectations
+        # households depends on population and has an oth_index_1 but only has data for 2009
+        households = demand.drivers[1]
+        self.assertEqual(households.name, "households")
+        self.assertEqual(households.base_driver_id, 2)
+        self.assertEqual(households.other_index_1_id, 1)
+        self.assertEqual(households.raw_values.index.names, [u'census division', u'housing types', u'year'])
+        # I know, the str() here is awkward, but levels is a pandas.core.base.FrozenList and I can't find a simple
+        # way to convert it to a list that UnitTest assertions will understand. This is good enough for now.
+        self.assertEqual(str(households.raw_values.index.levels), "[[56, 57, 58, 59, 60, 61, 62, 63, 64], [28, 29, 30], [2009]]")
+        self.assertEqual(households.raw_values.columns.values.tolist(), ['value'])
+        self.assertEqual(len(households.raw_values), 27)
+        self.assertEqual(households.raw_values.sum().value, 113616229.0)
+
+        # population does not depend on anything and has no oth_indexes but has data for many years
+        population = demand.drivers[2]
+        self.assertEqual(population.name, "population")
+        self.assertIsNone(population.base_driver_id)
+        self.assertIsNone(population.other_index_1_id)
+        self.assertEqual(population.raw_values.index.names, [u'census division', u'year'])
+        # See households section above for excuses for making this a string comparison
+        self.assertEqual(str(population.raw_values.index.levels), "[[56, 57, 58, 59, 60, 61, 62, 63, 64], [1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024, 2025, 2026, 2027, 2028, 2029, 2030, 2031, 2032, 2033, 2034, 2035, 2036, 2037, 2038, 2039, 2040]]")
+        self.assertEqual(population.raw_values.columns.values.tolist(), ['value'])
+        self.assertEqual(len(population.raw_values), 459)
+        self.assertEqual(population.raw_values.sum().value, 16327762200.0)
+
+        # TODO: figure out what else to assert here
         # obviously want to check for specific sectors, subsectors, measures, etc. but all are database dependent
         # Since htis is running against the US model at the moment, we can check for US-specific items.
         # Would be useful to have repr defined for Sector, SubSector, Measure, etc. so prints make more sense.
