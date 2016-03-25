5e3adc0c6cd8a8810d819d35d38940bea0bab507
Sam Borgeson <sborgeson@berkeley.edu>
2016-03-24 17:05:09 -0700

    recovery from dumb head merge?

--- energyPATHWAYS/orm/test_sqlalchemy.py
+++ energyPATHWAYS/orm/test_sqlalchemy.py
@@ -6,8 +6,6 @@ from sqlalchemy.orm import sessionmaker, backref, validates, reconstructor
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
-from sqlalchemy.sql import select # for fast core queries
-
 from data_provider import engine, Session
 import sys, time
 import datetime as dt
@@ -19,6 +17,8 @@ import cProfile
 import StringIO
 import pstats
 import contextlib
+
+from alchemy_util import RawDataHelp
 
 Base = declarative_base()
 metadata = Base.metadata
@@ -61,28 +61,6 @@ class TestData(Base):
     floatVal  = sa.Column( sa.Float )
     date      = sa.Column( sa.DateTime )
 
-class RawDataHelp():
-
-    @classmethod
-    def getAllRawData(cls):
-        if( not hasattr(cls,'all_raw_data') or cls.all_raw_data is None):
-            dataClass = '%sData' % ( cls.__tablename__ )
-            print '[RawDataHelp] loading all raw data for class %s' % (dataClass)
-            # try/except for when the underlying data class in not defined
-            try:
-                s = select([ globals()[dataClass] ])
-                cls.all_raw_data = pd.read_sql_query(s,engine)
-            except KeyError as ke: # TODO: trap and warn or allow to become an error?
-                raise( ValueError( '%s not found as a defined class has it been added to SQLAlchemy?' % (dataClass) ) )
-        return( cls.all_raw_data )
-
-    def getRawData(self):
-        if( self.id is None): raise ValueError('Class id must be set to get raw data')
-
-        if( not hasattr(self,'raw_data') or self.raw_data is None):
-            allRaw = self.getAllRawData()
-            self.raw_data = allRaw.loc[ allRaw['parent_id'] == self.id]
-        return self.raw_data
         
 class Test(Base, RawDataHelp):
     __tablename__ = 'Test'
--- us_model_example/main.py
+++ us_model_example/main.py
@@ -6,13 +6,10 @@ import cPickle as pickle
 import os
 import energyPATHWAYS
 from energyPATHWAYS import *
-<<<<<<< HEAD
-cfg = energyPATHWAYS.cfg
-=======
+
 #cfg = energyPATHWAYS.config
 from energyPATHWAYS.shape import shapes
 import ipdb
->>>>>>> 698734a... heading towards unit testing of config
 
 directory = os.getcwd()
 
@@ -27,45 +24,6 @@ resolve_demand = True
 #resolve the supply-side. A completed supply-side model must be saved. 
 resolve_supply = True
 ###########
-
-<<<<<<< HEAD
-if __name__ == "__main__":
-    if resolve_demand and resolve_supply:
-        model = energyPATHWAYS.PathwaysModel(cfgfile_path, custom_pint_definitions_path)
-        model.configure_energy_system()
-        model.populate_energy_system()
-        model.populate_measures()
-        model.calculate_demand_only()
-        with open(os.path.join(directory, 'model.p'), 'wb') as outfile:
-            pickle.dump(model, outfile, pickle.HIGHEST_PROTOCOL)
-        model.pass_results_to_supply()
-        model.calculate_supply()
-        model.supply.calculate_loop()
-        if save_models:
-            with open(os.path.join(directory, 'full_model_run.p'), 'wb') as outfile:
-                pickle.dump(model, outfile, pickle.HIGHEST_PROTOCOL)
-    elif resolve_demand and not resolve_supply: 
-        raise ValueError('Cant resolve demand and not resolve supply')
-    elif resolve_supply and not resolve_demand:
-        with open(os.path.join(directory, 'model.p'), 'rb') as infile:
-            model = pickle.load(infile)
-        model.model_config(db_path, cfgfile_path, custom_pint_definitions_path)
-        model.pass_results_to_supply()
-        model.calculate_supply()
-        model.supply.calculate_loop()
-        if save_models:
-            with open(os.path.join(directory, 'full_model_run.p'), 'wb') as outfile:
-                pickle.dump(model, outfile, pickle.HIGHEST_PROTOCOL)
-    else:
-        with open(os.path.join(directory, 'full_model_run.p'), 'rb') as infile:
-            model = pickle.load(infile)
-        model.model_config(db_path, cfgfile_path, custom_pint_definitions_path)   
-    model.supply.calculate_supply_outputs()
-    model.pass_results_to_demand()
-    model.calculate_combined_results()
-    model.export_results()
-
-=======
 
 with ipdb.launch_ipdb_on_exception():
     if __name__ == "__main__":
@@ -86,28 +44,21 @@ with ipdb.launch_ipdb_on_exception():
         elif resolve_demand and not resolve_supply: 
             raise ValueError('Cant resolve demand and not resolve supply')
         elif resolve_supply and not resolve_demand:
-            for scenario_id in model.scenario_dict.keys():
-                with open(os.path.join(directory, str(scenario)+'model.p'), 'rb') as infile:
-                    model = pickle.load(infile)
-                model.model_config(db_path, cfgfile_path, custom_pint_definitions_path)
-                model.pass_results_to_supply()
-                model.calculate_supply()
-                model.supply.calculate_loop()
-                if save_models:
-                    with open(os.path.join(directory, str(scenario_id)+'full_model_run.p'), 'wb') as outfile:
-                        pickle.dump(model, outfile, pickle.HIGHEST_PROTOCOL)
-                model.supply.calculate_supply_outputs()
-                model.pass_results_to_demand()
-                model.calculate_combined_results()
-                model.export_results(append_results)
+            with open(os.path.join(directory, 'model.p'), 'rb') as infile:
+                model = pickle.load(infile)
+            model.model_config(db_path, cfgfile_path, custom_pint_definitions_path)
+            model.pass_results_to_supply()
+            model.calculate_supply()
+            model.supply.calculate_loop()
+            if save_models:
+                with open(os.path.join(directory, 'full_model_run.p'), 'wb') as outfile:
+                    pickle.dump(model, outfile, pickle.HIGHEST_PROTOCOL)
         else:
-            for scenario_id in model.scenario_dict.keys():
-                with open(os.path.join(directory, str(scenario_id)+'full_model_run.p'), 'rb') as infile:
-                    model = pickle.load(infile)
-                model.model_config(db_path, cfgfile_path, custom_pint_definitions_path)
-                model.supply.calculate_supply_outputs()
-                model.pass_results_to_demand()
-                model.calculate_combined_results()
-                model.export_results(append_results)
-                
->>>>>>> 698734a... heading towards unit testing of config
+            with open(os.path.join(directory, 'full_model_run.p'), 'rb') as infile:
+                model = pickle.load(infile)
+            model.model_config(db_path, cfgfile_path, custom_pint_definitions_path)   
+        model.supply.calculate_supply_outputs()
+        model.pass_results_to_demand()
+        model.calculate_combined_results()
+        model.export_results()
+
