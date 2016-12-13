a83f5e3ca5ba10c9fc12e2cdc4b0b6d41cb6d599
Michael A Cohen <mcohen@rollingturtle.com>
2016-12-12 16:59:48 -0800

    Daemonize queue_monitor
    Also, fix a bug where it would leave zombie processes around
    after model runs finished.

--- energyPATHWAYS/api/queue_monitor.py
+++ energyPATHWAYS/api/queue_monitor.py
@@ -1,38 +1,70 @@
 import sys
+import os
 import logging
+import pwd
+import grp
 import datetime
 import click
 from flask import Flask
-import multiprocessing
 import time
 import psutil
 import sqlalchemy
 import subprocess
+import daemon
+import daemon.pidfile
 import models
+
+# set up logging
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename='/var/log/queue_monitor/queue_monitor_%s.log' %
+                             datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S.%f%z'),
+                    level=logging.INFO,
+                    format='%(asctime)s %(levelname)-8s %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S')
+
+
+def handle_exception(exc_type, exc_value, exc_traceback):
+    if issubclass(exc_type, KeyboardInterrupt):
+        sys.__excepthook__(exc_type, exc_value, exc_traceback)
+        return
+
+    logger.critical("Uncaught exception", exc_info=(exc_type, exc_value, exc_traceback))
+
+sys.excepthook = handle_exception
+
 
 # create the application object
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 models.db.init_app(app)
 
-# set up logging
-logger = logging.getLogger(__name__)
-logging.basicConfig(filename='../../us_model_example/queue_monitor_%s.log' %
-                             datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S.%f%z'),
-                    level=logging.INFO,
-                    format='%(asctime)s %(levelname)-8s %(message)s',
-                    datefmt='%Y-%m-%d %H:%M:%S')
 
 @click.command()
 @click.option('-f', '--poll-frequency', default=60,
               help="Number of seconds to wait between queue checks. Note that the actual interval between the start "
                    "of one check and the next will be slightly longer than this because of the time taken to actually "
                    "check the queue. Default 60.")
-@click.option('-w', '--max-workers', default=4,
-              help="Maximum number of simultaneous scenario runs. Defaults to one.")
-def start_queue_monitor(poll_frequency, max_workers):
-    qm = QueueMonitor(poll_frequency, max_workers)
-    qm.start()
+@click.option('-w', '--max-workers', default=2,
+              help="Maximum number of simultaneous scenario runs. Defaults to two.")
+@click.option('-u', '--user', default='www-data',
+              help="User to run queue_monitor as. Defaults to 'www-data'. When testing on macOS, use 'daemon'.")
+@click.option('-g', '--group',
+              help="Group to run queue_monitor as. Defaults to the group of the selected user.")
+@click.option('-d', '--directory', default='/var/www/energyPATHWAYS/us_model_example',
+              help="Working directory for the monitor; should be the working directory for the energyPATHWAYS model.")
+def start_queue_monitor(poll_frequency, max_workers, user, group):
+    pw = pwd.getpwnam(user)
+    gid = pw.pw_gid if group is None else grp.getgrnam(group).gr_gid
+
+    with daemon.DaemonContext(
+        files_preserve=[logging.root.handlers[0].stream.fileno()],
+        pidfile=daemon.pidfile.PIDLockFile('/var/run/queue_monitor.pid'),
+        uid=pw.pw_uid,
+        gid=gid
+    ):
+        logger.info('My process id is %i' % os.getpid())
+        qm = QueueMonitor(poll_frequency, max_workers)
+        qm.start()
 
 class QueueMonitor:
     ACTIVE_STATUS_IDS = [models.ScenarioRunStatus.LAUNCHED_ID, models.ScenarioRunStatus.RUNNING_ID]
@@ -40,6 +72,7 @@ class QueueMonitor:
     def __init__(self, poll_frequency, max_workers):
         self.poll_frequency = poll_frequency
         self.max_workers = max_workers
+        self.child_procs = []
         logger.info('Starting queue monitor polling every %i seconds with %i workers' %
                     (self.poll_frequency, self.max_workers))
 
@@ -59,6 +92,15 @@ class QueueMonitor:
         return models.ScenarioRun.query.filter(models.ScenarioRun.status_id.in_(self.ACTIVE_STATUS_IDS))
 
     def check_for_lost_runs(self):
+        logger.debug("Polling for completed child processes")
+        terminated_procs = []
+        for proc in self.child_procs:
+            if proc.poll() is not None:
+                logger.info("Process %i terminated with return code %i" % (proc.pid, proc.returncode))
+                terminated_procs.append(proc)
+
+        self.child_procs = [proc for proc in self.child_procs if proc not in terminated_procs]
+
         logger.debug("Checking for lost runs")
 
         # Find the ids of any runs that claim to be running but whose pids are no longer active.
@@ -119,9 +161,10 @@ class QueueMonitor:
             models.db.session.commit()
             # Actually run the scenario
             proc = subprocess.Popen(['energyPATHWAYS', '-a',
-                                     '-p', '../../us_model_example',
+                                     '-p', '.',
                                      '-s', str(run.scenario_id),
                                      '--no_save_models'])
+            self.child_procs.append(proc)
             # Record the pid
             logger.info("ScenarioRun %i got pid %i" % (run.id, proc.pid))
             run.pid = proc.pid
