cc8a5ceeb70a8cce118d9d0e1b8c70316f9ff6f5
Michael A Cohen <mcohen@rollingturtle.com>
2017-03-17 13:44:10 -0700

    Add scenario descriptions to dump

--- model_building_tools/db_to_file/extract_scenario_tree.py
+++ model_building_tools/db_to_file/extract_scenario_tree.py
@@ -1,7 +1,44 @@
 import psycopg2.extras
 import json
-from collections import defaultdict, OrderedDict
+from collections import OrderedDict, Callable
 import energyPATHWAYS.config as cfg
+
+class DefaultOrderedDict(OrderedDict):
+    # Source: http://stackoverflow.com/a/6190500/562769
+    def __init__(self, default_factory=None, *a, **kw):
+        if (default_factory is not None and
+           not isinstance(default_factory, Callable)):
+            raise TypeError('first argument must be callable')
+        OrderedDict.__init__(self, *a, **kw)
+        self.default_factory = default_factory
+
+    def __getitem__(self, key):
+        try:
+            return OrderedDict.__getitem__(self, key)
+        except KeyError:
+            return self.__missing__(key)
+
+    def __missing__(self, key):
+        if self.default_factory is None:
+            raise KeyError(key)
+        self[key] = value = self.default_factory()
+        return value
+
+    def __reduce__(self):
+        if self.default_factory is None:
+            args = tuple()
+        else:
+            args = self.default_factory,
+        return type(self), args, None, None, self.items()
+
+    def copy(self):
+        return self.__copy__()
+
+    def __copy__(self):
+        return type(self)(self.default_factory, self)
+
+    def __deepcopy__(self, memo):
+        import copy
 
 cfg.init_cfgfile('../../model_runs/us_model_example/config.INI')
 cfg.init_db()
@@ -29,7 +66,8 @@ package_data_tables = {DEMAND: {
 }
 
 # Prepare a nested lookup for which measure_ids go with which packages
-packages = {DEMAND: defaultdict(lambda: defaultdict(list)), SUPPLY: defaultdict(lambda: defaultdict(list))}
+packages = {DEMAND: DefaultOrderedDict(lambda: DefaultOrderedDict(list)),
+            SUPPLY: DefaultOrderedDict(lambda: DefaultOrderedDict(list))}
 for side in SIDES:
     for table in package_data_tables[side].values():
         cur.execute("""
@@ -43,6 +81,7 @@ for side in SIDES:
 # Queries
 queries = {DEMAND: """
     SELECT "Scenarios".id AS scenario_id, "Scenarios".name AS scenario_name,
+           "Scenarios".description AS scenario_description,
            "DemandCases".id AS demand_case_id, "DemandCases".name AS demand_case_name,
            "DemandSubsectors".name AS sector_or_node_name,
            "DemandStates".*
@@ -56,12 +95,16 @@ queries[SUPPLY] = queries[DEMAND].replace('DemandSubsectors', 'SupplyNodes').rep
                   'supply_node_id').replace('demand', 'supply').replace('Demand', 'Supply')
 
 # Populate hierarachical data structure for scenarios
-scenarios = defaultdict(lambda: defaultdict(defaultdict))
+scenarios = DefaultOrderedDict(lambda: DefaultOrderedDict(DefaultOrderedDict))
 
 for side in SIDES:
     cur.execute(queries[side])
     case_name_col = "{}_case_name".format(side.lower(),)
     for row in cur.fetchall():
+        # Yes, this value is unnecessarily set repeatedly (one time for each State within the Scenario)
+        # but it's not worth optimizing
+        scenarios[row['scenario_name']]['description'] = row['scenario_description']
+
         state = OrderedDict({'description': row['description']})
         for id_col, table_name in package_data_tables[side].iteritems():
             if row[id_col] is not None:
@@ -71,7 +114,7 @@ for side in SIDES:
 
 # Output to json
 for name, content in scenarios.iteritems():
-    file_name = name.replace(' ', '_') + '.json'
+    file_name = name.replace(' ', '_').lower() + '.json'
     print "Writing {}".format(file_name,)
     with open(file_name, 'w') as outfile:
         json.dump({name: content}, outfile, indent=4, separators=(',', ': ')) # sort_keys=True,
