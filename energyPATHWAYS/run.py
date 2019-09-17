463a7618d42e478cfb89ba2b327cd4ebd638ebf8
Ryan Jones <ryandrewjones@gmail.com>
2019-09-17 15:41:14 -0600

    updated to work with rio scenarios from scenario builder

--- energyPATHWAYS/run.py
+++ energyPATHWAYS/run.py
@@ -32,34 +32,7 @@ import energyPATHWAYS.unit_converter as unit_converter
 model = None
 run_start_time = time.time()
 
-# Deprecated
-# def myexcepthook(exctype, value, tb):
-#     logging.error("Exception caught during model run.", exc_info=(exctype, value, tb))
-#     # It is possible that we arrived here due to a database exception, and if so the database will be in a state
-#     # where it is not accepting commands. To be safe, we do a rollback before attempting to write the run status.
-#     try:
-#         cfg.con.rollback()
-#     except (psycopg2.InterfaceError, AttributeError):
-#         logging.exception("Unable to rollback database connection while handling an exception, "
-#                           "probably because the connection hasn't been opened yet or was already closed.")
-#     sys.__excepthook__(exctype, value, tb)
-# sys.excepthook = myexcepthook
-#
-# def signal_handler(signal, frame):
-#     logging.error('Execution interrupted by signal ' + str(signal))
-#     # As documented here,
-#     # http://stackoverflow.com/questions/39215527/psycopg2-connection-unusable-after-selects-interrupted-by-os-signal
-#     # The normal database connection may be in an unusable state here if the signal arrived while the connection
-#     # was in the middle of performing some work. Therefore, we re-initialize the connection so that we can use it to
-#     # write the cancelation status to the db.
-#     cfg.init_db()
-#     sys.exit(0)
-# signal.signal(signal.SIGINT, signal_handler)
-# signal.signal(signal.SIGTERM, signal_handler)
-
 @click.command()
-@click.option('-p', '--path', type=click.Path(exists=True), help='Working directory for energyPATHWAYS run.')
-@click.option('-c', '--config', default='config.INI', help='File name for the energyPATHWAYS configuration file.')
 @click.option('-s', '--scenario', type=str, multiple=True, help='Scenario name to run. More than one can be specified by repeating this option. If none are specified, the working directory will be scanned for .json files and all will be run.')
 @click.option('-ld', '--load_demand/--no_load_demand', default=False, help='Load a cached model with the demand side complete.')
 @click.option('--solve_demand/--no_solve_demand', default=True, help='Solve the demand side of the model.')
@@ -67,18 +40,15 @@ run_start_time = time.time()
 @click.option('--solve_supply/--no_solve_supply', default=True, help='Solve the supply side of the model.')
 @click.option('--load_error/--no_load_error', default=False, help='Load the error pickle of a previous model run that crashed.')
 @click.option('--export_results/--no_export_results', default=True, help='Write results from the demand and supply sides of the model.')
-@click.option('--pickle_shapes/--no_pickle_shapes', default=True, help='Cache shapes after processing.')
 @click.option('--save_models/--no_save_models', default=True, help='Cache models after running.')
-@click.option('--log_name', help='File name for the log file.')
 @click.option('--clear_results/--no_clear_results', default=False, help='Should results be cleared or appended when starting a new model run')
 @click.option('--subfolders/--no_subfolders', default=False, help='Most users do not need this! Used when a system of subfolders has been created for different runs')
-def click_run(path, config, scenario, load_demand, solve_demand, load_supply, solve_supply, load_error, export_results, pickle_shapes, save_models, log_name, clear_results, subfolders):
-    run(path, config, scenario, load_demand, solve_demand, load_supply, solve_supply, load_error, export_results, pickle_shapes, save_models, log_name, clear_results, subfolders)
+@click.option('-r', '--rio_scenario', type=str, multiple=True, help='RIO scenario name to run. More than one can be specified by repeating this option.')
+def click_run(scenario, load_demand, solve_demand, load_supply, solve_supply, load_error, export_results, save_models, clear_results, subfolders, rio_scenario):
+    run(scenario, load_demand, solve_demand, load_supply, solve_supply, load_error, export_results, save_models, clear_results, subfolders, rio_scenario)
 
-def run(path, config, scenarios, load_demand=False, solve_demand=True, load_supply=False, solve_supply=True, load_error=False,
-        export_results=True, pickle_shapes=True, save_models=True, log_name=None, clear_results=False, subfolders=False,rio_scenario=None):
+def run(scenarios, load_demand=False, solve_demand=True, load_supply=False, solve_supply=True, load_error=False, export_results=True, save_models=True, clear_results=False, subfolders=False, rio_scenario=None):
     global model
-    path = os.getcwd() if path is None else path
     cfg.initialize_config()
     if not subfolders:
         GeoMapper.get_instance().log_geo()
@@ -90,11 +60,9 @@ def run(path, config, scenarios, load_demand=False, solve_demand=True, load_supp
         if not scenarios:
             raise ValueError, "No scenarios specified and no .json files found in working directory."
 
-    # Users may have specified a scenario using the full filename, but for our purposes the 'id' of the scenario
-    # is just the part before the .json
     scenarios = util.ensure_iterable(scenarios)
     scenarios = [os.path.splitext(s)[0] for s in scenarios]
-    rio_scenario = [None]*len(scenarios) if rio_scenario is None else util.ensure_iterable(rio_scenario)
+    rio_scenario = [None]*len(scenarios) if rio_scenario is None or not len(rio_scenario) else util.ensure_iterable(rio_scenario)
 
     combined_scenarios = zip(scenarios, rio_scenario)
     logging.info('Scenario run list: {}'.format(', '.join(scenarios)))
@@ -102,7 +70,7 @@ def run(path, config, scenarios, load_demand=False, solve_demand=True, load_supp
         if subfolders:
             logging.shutdown()
             logging.getLogger(None).handlers = []
-            cfg.initialize_config(os.path.join(path, scenario), config, log_name)
+            cfg.initialize_config()
             logging.info('SUBFOLDERS ARE IN USE')
             GeoMapper.get_instance().log_geo()
         scenario_start_time = time.time()
@@ -171,18 +139,16 @@ class SubsectorPerturbation(object):
 if __name__ == "__main__":
     workingdir = r'C:\Github\EnergyPATHWAYS_scenarios\UCS'
     os.chdir(workingdir)
-    config = 'config.INI'
     rio_scenario = None
     scenario = ['UCS']
 
-    run(workingdir, config, scenario,
-    load_demand   = True,
+    run(scenario,
+    load_demand   = False,
     solve_demand  = True,
     load_supply   = False,
     solve_supply  = True,
     export_results= True,
     load_error    = False,
-    pickle_shapes = True,
     save_models   = True,
     clear_results = True,
     rio_scenario=rio_scenario)
