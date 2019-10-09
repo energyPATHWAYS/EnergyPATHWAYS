4b3c0fca88dfb4d995bf148e2afe4ad01f607d68
Ben <ben.haley@evolved.energy>
2019-10-09 15:02:23 -0400

    changed dual fuel efficiency consumption type from integer to text

--- energyPATHWAYS/supply_technologies.py
+++ energyPATHWAYS/supply_technologies.py
@@ -367,7 +367,7 @@ class SupplyTechEfficiency(schema.SupplyTechsEfficiency):
             self.values = self.values.unstack('year')
             self.values.columns = self.values.columns.droplevel()
             self.values = pd.concat([self.values] *len(self.vintages),keys=self.vintages,names=['vintage'])
-            self.values['efficiency_type'] = 1
+            self.values['efficiency_type'] = 'consumed'
             self.values = self.values.set_index('efficiency_type',append=True)
         elif self._has_data and self.raw_values is not None:
             self.convert()
