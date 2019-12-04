e8b28610262d5528155db63cbc0665f220224130
Richard Plevin <rich@plevin.com>
2019-12-03 17:10:18 -0800

    WIP -- validation updates

--- energyPATHWAYS/__init__.py
+++ energyPATHWAYS/__init__.py
@@ -6,5 +6,13 @@ import util
 import dispatch_classes
 import pathways_model
 
+#
+# These methods form the API between a subclass of CsvDatabase and the validation subsystem.
+#
+def get_metadata():
+    from .generated.new_database import _Metadata
+    return _Metadata
 
-
+def database_class():
+    from .generated.new_database import EnergyPathwaysDatabase
+    return EnergyPathwaysDatabase
--- model_building_tools/gen_classes/merge-validation-data.py
+++ model_building_tools/gen_classes/merge-validation-data.py
@@ -2,37 +2,66 @@ from __future__ import print_function
 
 import pandas as pd
 
-metaDF = pd.read_csv("validation-metadata.csv")
-commDF = pd.read_csv("column-comments.csv")
+df = pd.read_csv("validation-metadata.csv").fillna(value='')
 
-# metaDF.set_index(['table_name', 'column_name'], inplace=True)
-# commDF.set_index(['table_name', 'column_name'], inplace=True)
+df.loc[df.column_name == 'id', 'column_name'] = 'name'
+df.loc[df.foreign_column == 'id', 'foreign_column'] = 'name'
 
-# Add comment column
-#metaDF['comment'] = ''
+# Remove "_id" from column names
+for idx, row in df.iterrows():
+    col_name = row.column_name
+    if col_name.endswith('_id'):
+        df.loc[idx, 'column_name'] = col_name[:-3]
 
-df = metaDF.merge(commDF, how='outer', on=['table_name', 'column_name']).fillna(value='')
-
-# Rename columns
-df.rename(columns={'foreign_table' : 'referenced_table',
-                   'foreign_column': 'referenced_field'}, inplace=True)
+    col_name = row.foreign_column
+    if col_name.endswith('_id'):
+        df.loc[idx, 'foreign_column'] = col_name[:-3]
 
 # Create and populate "not_null" and "cascade_delete" columns
-df['not_null'] = 'NO'
-df.loc[df.nullable == 'NO', 'not_null'] = 'YES'
+df['not_null'] = 'FALSE'
+df.loc[df.nullable == 'NO', 'not_null'] = 'TRUE'
 
-df['cascade_delete'] = 'NO'
-df.loc[df.on_del == 'CASCADE', 'cascade_delete'] = 'YES'
+df['cascade_delete'] = 'FALSE'
+df.loc[df.on_del == 'CASCADE', 'cascade_delete'] = 'TRUE'
+
+# Eliminate duplicate rows for which there are table-independent column specification
+manualDF = pd.read_csv("manual-validation-info.csv").fillna(value='')
+
+genericCols = manualDF.loc[manualDF.table_name == ''].column_name.unique()
+
+# Drop table-specific entries for columns with table-independent entries only
+# if all the "nullable" values are the same, and set the "not_null" value in
+# the manualDF to the value found in the postgres metadata.
+dropable = []
+for col in genericCols:
+    values = df.query("column_name == @col").not_null.unique()
+    if len(values) == 1:
+        dropable.append(col)
+        manualDF.loc[manualDF.column_name == col, 'not_null'] = values[0]
+
+df = df.query("column_name not in @dropable").copy()
 
 # Add other columns as empty placeholders
 for col in ['linked_column', 'dtype', 'folder', 'additional_valid_inputs']:
     df[col] = ''
 
-df.drop(['on_del', 'nullable'], axis='columns', inplace=True)
+df.rename(columns={'foreign_table': 'ref_tbl', 'foreign_column' : 'ref_col'}, inplace=True)
 
-# Re-order columns
-cols = ['table_name', 'column_name', 'not_null', 'linked_column', 'dtype', 'folder', 'referenced_table',
-        'referenced_field', 'cascade_delete', 'comment', 'additional_valid_inputs']
+# Re-order columns and drop unneeded ones
+cols = ['table_name', 'column_name', 'not_null', 'linked_column', 'dtype', 'folder', 'ref_tbl',
+        'ref_col', 'cascade_delete', 'additional_valid_inputs']
 
 df = df[cols]
+
+# Add as many "Unnamed: X" columns as occur in the manualDF
+for col in manualDF.columns:
+    if col.startswith('Unnamed:'):
+        df[col] = ''
+
+df = manualDF.append(df)
+
+prefix = 'Unnamed: '
+count = len(prefix)
+df.rename(columns=lambda col: '_c_' + col[count:] if col.startswith(prefix) else col, inplace=True)
+
 df.to_csv('merged-validation-data.csv', index=None)
